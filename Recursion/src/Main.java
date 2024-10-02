5df048d1c4b6221e4b722de4d45cddd2c4998b47
Abubakkar-Siddhiq <abubakkarsiddhiqk4@gmail.com>
2024-10-02 19:43:08 +0530

    Recursion brewing....

---
+++ Recursion/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    /*
+    Yo! Remember these Golden Rules while using Recursion 📜
+
+        1. Write a Break Statement/ Base Case.
+        2. Then, Write the recursive calls.
+
+    See, Recursion (Without base-case) is like a car. With no breaks, It leads to accidents (Stack-Overflow Errors 🥴).
+    So, Add break First. Then write a logic on when the break has to be applied.
+    Then, And only then, Write the recursive calls.
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
