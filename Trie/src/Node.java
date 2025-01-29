3efe1f2d7b2de257b4b8409b84d24a72f2eafd3d
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2025-01-29 21:58:31 +0530

    Added Trie

--- Trie/src/Node.java
+++ Trie/src/Node.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Node {
+    public char c;
+    public Node[] children;
+    public boolean isWord;
+
+    public Node(char c) {
+        this.c = c;
+        this.children = new Node[26];
+        this.isWord = false;
+    }
 }
--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Trie {
+
+    private final Node root;
+
+    public Trie() {
+        root = new Node('\0');
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new Node(c);
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isWord = true;
+    }
+
+    public boolean search(String word) {
+        Node node = getNode(word);
+        return node != null && node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        return getNode(prefix) != null;
+    }
+
+    public Node getNode(String word) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new Node(c);
+            }
+
+            curr = curr.children[c - 'a'];
+        }
+        return curr;
+    }
+
 }
